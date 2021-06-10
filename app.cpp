73adeb061632e9a882d7bfa18873f1f0e3d7d3b4
Ugo <okekeugochukwu.dev@gmail.com>
2021-06-10 03:42:34 +0100

    initial commmit

---
+++ app.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int initialAmount, userChoice, amtToDeposit, amtToWithdraw;
+    cout << "Starting Program.." << endl;
+
+    cout << "Enter amount" << endl;
+    cin >> initialAmount ;
+
+    cout << "Press one for deposit" << endl;
+    cout << "Press two for withdrawal" << endl;
+    cin >> userChoice;
+
+    if (userChoice == 1)
+    {
+        cout << "Enter amount to deposit" << endl;
+        cin >> amtToDeposit ;
+
+        cout << "New balance is " << amtToDeposit + initialAmount << endl;
+    }
+    else if(userChoice == 2) {
+        cout << "Enter amount to withdraw" << endl;
+        cin >> amtToWithdraw ;
+
+        cout << "New balance is " << amtToDeposit + initialAmount << endl;
+    }
+    else {
+        cout << "Invalid input" << endl;
+    }
+    return 0;
+    
+}
